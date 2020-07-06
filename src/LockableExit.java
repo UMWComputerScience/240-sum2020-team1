bd81e775547f6cbdd8ef6702475d6a83cb8de836
1434707 <1434707@gmail.com>
2020-07-06 03:52:10 +0000

    add api comments to LockableExit

---
+++ src/LockableExit.java
@@ -0,0 +1,54 @@
+/**
+ * A LockableExit is a type of exit (extends Exit)
+ * that can be manipulated by being able to be locked,
+ * unlocked, or permanentaly destroyed. 
+ *
+ * the instance variables isLocked and isDestroyed are checked by 
+ * the movement command to make sure the played doesn't move
+ * through an unusable exit. 
+ */
+class LockableExit extends Exit{
+	public boolean isLocked;
+	public boolean isDestroyed;
+	
+	/**
+	 * checks the status of an exit, therefore isLocked and
+	 * isDestroyed are checked by the moveCommand class to
+	 * see if the exit can be used.
+	 *
+	 * returns the boolean values of isLocked and isDestroyed
+	 */
+	public boolean getStatus(){
+
+	}
+
+	/**
+	 * changes the isLocked boolean value to true
+	 *
+	 * is called by the Room class when a new CloseRoomCommand
+	 * is instantiated.
+	 * */
+	public void lockExit(){
+
+	}
+
+	/**
+	 * changes the isLocked boolean value to false
+	 *
+	 * is called by the Room class when a new OpenRoomCommand
+	 * is instantiated.
+	 * */
+	public void unlockExit(){
+
+	}
+	
+	/**
+	 * changes the isDestroyed boolean value to true
+	 * 
+	 * is called by the Room class when a new CloseRoomCommand
+	 * is instantiated.
+	 * */
+	public void destroyExit(){
+
+	}
+}
