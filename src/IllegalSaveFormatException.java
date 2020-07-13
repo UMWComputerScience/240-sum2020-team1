1972cff745afd1d1237b80b388c36f28b94fed25
Michael <mcividan@email.umw.edu>
2020-07-13 05:14:27 +0000

    merging branches -MGC

--- src/IllegalSaveFormatException.java
+++ src/IllegalSaveFormatException.java
@@ -1,10 +1,4 @@
 
 /** exception to be thrown if an invalid or incompatible .sav file is detected.
 */
- class IllegalSaveFormatException extends Exception {
-/**Declares exception.
- * */
-	public IllegalSaveFormatException(String e) {
-            super(e);
-        }
-    }
+ class IllegalSaveFormatException extends Exception {}
