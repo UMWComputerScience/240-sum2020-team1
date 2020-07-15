bcd5fac673a06ec0936d31e07ec003d7cfbd8b85
1434707 <1434707@gmail.com>
2020-07-15 02:33:14 +0000

    ScoreEvent worked on

--- src/ScoreEvent.java
+++ src/ScoreEvent.java
@@ -1,12 +1,19 @@
-class ScoreEvent extends Event{
-	private int value;
+import java.util.Arrays;
 
-	ScoreEvent(int value){
-		this.value = value;
+class ScoreEvent extends Event{
+	private int scoreVal;
+
+	ScoreEvent(String s){
+		String [] splitS = s.split("(");
+		String unparsedNum = splits[1];
+		String [] splitNum = unparsedNum.split(")");
+		String tempScoreVal = splitNum[0];
+		int tempIntScoreVal = Integer.parseInt(tempScoreVal);
+		this.scoreVal = tempIntScoreVal;	
 	}
 
 	String callEvent(){
-		GameState.instance().increaseScore(value);
-		return "";
+		GameState.instance().increaseScore(scoreVal);
+		return "";		
 	}
 }
