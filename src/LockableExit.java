f1bb49a3577b85d174ef0bbb076d7aba0fbe644e
1434707 <1434707@gmail.com>
2020-07-22 21:08:44 +0000

    LockableExit complete

--- src/LockableExit.java
+++ src/LockableExit.java
@@ -14,27 +14,32 @@ class LockableExit extends Exit{
 	 * currently used.
 	 */
 	public boolean getStatus(){
-	    return true;
+	    if(this.isLocked==true || this.isDestroyed==true){
+	    	return false;
+	    }
+	    else{
+	    	return true;
+	    }
 	}
 
 	/**
 	 * Changes the condition of an exit to be locked.
 	 * Note that this prevents the user from usimg this exit.*/
 	public void lockExit(){
-
+		this.isLocked = true;
 	}
 
 	/**
 	 * Changes the condition of an exit to be unlocked.
 	 * Note that this allows the user to use this exit.*/
 	public void unlockExit(){
-
+		this.isLocked = false;
 	}
 	
 	/**
 	 * Changes the condition of an exit to be destroyed. 
 	 * Note that this exit can no longer be used once destroyed.*/
 	public void destroyExit(){
-
+		this.isDestroyed = true;
 	}
 }
