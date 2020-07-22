e4c6b9554d76ffb619b7ad3a155d0fded8468320
Michael <mcividan@email.umw.edu>
2020-07-22 15:54:33 +0000

    added maxLoadException.java

---
+++ src/maxLoadException.java
@@ -0,0 +1,4 @@
+/** Exception to be thrown is the player attempts to add an item to their inventory that would cause their total weight to exceed the returned value of getStrength() 
+*/
+
+public class maxLoadException extends Exception {}
